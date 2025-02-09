0c891fd0482183429447147aeaa0bdd686745b0a
Kikiparrot <morriss3241@masseyhigh.school.nz>
2025-02-10 11:01:40 +1300

    Update C_01_yes_basic.py

--- C_01_yes_basic.py
+++ C_01_yes_basic.py
@@ -1,16 +1,13 @@
-print("hello")
-
+error = "please enter an integer more than / equal to 13."
 while True:
-    want_instructions = input("do you want to see the instructions?").lower()
+    try:
+        game_goal = int(input("what is the game goal"))
 
-    # check the user says yes / no
-    if want_instructions == "yes" or want_instructions == "y":
-        print("you said yes")
-        break
-    elif want_instructions == "no" or want_instructions == "n":
-        print("you said no")
-        break
-    else:
-        print("please answer yes / no")
-        continue
-print("done :]")
+        if game_goal < 13:
+            print(error)
+        else:
+            print("game goal: {game_goal}")
+            break
+
+    except ValueError:
+        print(error)
